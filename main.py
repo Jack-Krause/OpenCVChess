16b6fcb056c370276763822463d976ed3d3afe85
Jack Krause <jmkrause@iastate.edu>
2025-02-09 14:34:46 -0600

    lines save

--- main.py
+++ main.py
@@ -15,15 +15,19 @@ img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 edges = cv2.Canny(img_gray, 100, 200)
 lines = lsd.detect(img_gray)
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-plt.show()
-for edge in edges:
-    print(edge)
+#plt.subplot(121),plt.imshow(img,cmap = 'gray')
+#plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+#plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+#plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+#plt.show()
+#for edge in edges:
+#    print(edge)
+
+    
+for line in lines:
+    round(line, 1)
     
     
-drawn_img = lsd.drawSegments(img_gray, lines[0])
+drawn_img = lsd.drawSegments(img_rgb, lines[0])
 plt.imshow(drawn_img, cmap="gray")
 plt.show()
