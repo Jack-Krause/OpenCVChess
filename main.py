bbe1eabf25b348be3d336001a0ce81e2eb61a95d
Jack Krause <jmkrause@iastate.edu>
2025-02-09 13:51:20 -0600

    edges

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 from matplotlib import pyplot as plt
+import numpy as np
   
 # Opening image
 img = cv2.imread("cb1.jpg")
@@ -10,35 +11,12 @@ img = cv2.imread("cb1.jpg")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
   
-  
-# Use minSize because for not 
-# bothering with extra-small 
-# dots that would look like STOP signs
-stop_data = cv2.CascadeClassifier('stop_data.xml')
-  
-found = stop_data.detectMultiScale(img_gray, 
-                                   minSize =(20, 20))
-  
-# Don't do anything if there's 
-# no sign
-amount_found = len(found)
-  
-if amount_found != 0:
-      
-    # There may be more than one
-    # sign in the image
-    for (x, y, width, height) in found:
-          
-        # We draw a green rectangle around
-        # every recognized sign
-        cv2.rectangle(img_rgb, (x, y), 
-                      (x + height, y + width), 
-                      (0, 255, 0), 5)
-        
-        print("location of square:", x, y, width, height)
-          
-# Creates the environment of 
-# the picture and shows it
-plt.subplot(1, 1, 1)
-plt.imshow(img_rgb)
+edges = cv2.Canny(img_gray, 100, 200)  
+print(edges)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+ 
 plt.show()
