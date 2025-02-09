6dff090ff5049449c227c22846f933bd2af99255
Jack Krause <jmkrause@iastate.edu>
2025-02-09 14:19:39 -0600

    finds lines

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
   
 # Opening image
 img = cv2.imread("cb1.jpg")
+lsd = cv2.createLineSegmentDetector(0)
   
 # OpenCV opens images as BRG 
 # but we want it as RGB We'll 
@@ -11,12 +12,18 @@ img = cv2.imread("cb1.jpg")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
   
-edges = cv2.Canny(img_gray, 100, 200)  
-print(edges)
+edges = cv2.Canny(img_gray, 100, 200)
+lines = lsd.detect(img_gray)
 
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(edges,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
- 
 plt.show()
+for edge in edges:
+    print(edge)
+    
+    
+drawn_img = lsd.drawSegments(img, lines[0])
+cv2.imshow("LSD", drawn_img)
+cv2.waitKey(0)
